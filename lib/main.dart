bb3e12c9c76a9a06e1d381529a2ee2600ad8de85
zsebe.akos <zsebe.akos@accenture.com>
2020-01-15 17:01:39 +0200

    debug enabled

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Workmanager.initialize(
     callbackDispatcher, // The top level function, aka callbackDispatcher
-    isInDebugMode: false // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
+    isInDebugMode: true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
   );
   SharedPrefs.getTheme().then((onValue) {
     runApp(EasyLocalization(
