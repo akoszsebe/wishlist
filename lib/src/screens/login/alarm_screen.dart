3c8dc76d8e9123830d160eaae5964b77db23af49
Akos Zsebe <zsebea@yahoo.com>
2020-01-21 00:35:59 +0200

    added dragable

--- lib/src/screens/login/alarm_screen.dart
+++ lib/src/screens/login/alarm_screen.dart
@@ -24,6 +24,8 @@ class _AlarmScreenState extends StateMVC<AlarmScreen> {
   LoginController _con;
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
+  Color barColor1 = Colors.transparent;
+  Color barColor2 = Colors.transparent;
   @protected
   @override
   void initState() {
@@ -38,60 +40,129 @@ class _AlarmScreenState extends StateMVC<AlarmScreen> {
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
     return Scaffold(
-      body: buildBody(themeProvider),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Padding(
-          padding: EdgeInsets.only(bottom: 60),
-          child: ButtonTheme(
-              height: 140,
-              minWidth: 140,
-              child: RaisedButton(
-                  child: Text(
-                    "Stop",
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                  color: Theme.of(context).accentColor,
-                  onPressed: () async {
-                    FlutterRingtonePlayer.stop();
-                    try {
-                      await platform
-                          .invokeMethod('finishAndRemoveTask');
-                    } on PlatformException catch (e) {}
-                    //SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+        key: scaffoldKey,
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: Draggable(
+          data: 5,
+          axis: Axis.horizontal,
+          child: buildFab(),
+          feedback: buildFab(),
+          childWhenDragging: Container(),
+          onDragCompleted: () async {
+            print("vege");
+            FlutterRingtonePlayer.stop();
+            try {
+              await platform.invokeMethod('finishAndRemoveTask');
+            } on PlatformException catch (e) {}
+          },
+        ),
+        body: buildBody(themeProvider));
+  }
+
+  bool accepted = false;
+  Widget buildBody(ThemeProvider themeProvider) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Text(
+            'Alarm',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            title.split(':')[1],
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 200),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                height: 250.0,
+                color: barColor1,
+                child: DragTarget(
+                  builder: (context, List<int> candidateData, rejectedData) {
+                    print(candidateData);
+                    return Center(
+                        child: Text(
+                      "<",
+                      style: TextStyle(color: Colors.white, fontSize: 22.0),
+                    ));
                   },
-                  shape: RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(70.0))))),
+                  onWillAccept: (data) {
+                    print("will");
+                    setState(() {
+                      barColor1 = Colors.red;
+                    });
+                    return true;
+                  },
+                  onAccept: (data) {
+                    setState(() {
+                      barColor1 = Colors.red;
+                    });
+                  },
+                ),
+              ),
+              Container(
+                width: 100.0,
+                height: 250.0,
+                color: barColor2,
+                child: DragTarget(
+                  builder: (context, List<int> candidateData, rejectedData) {
+                    return Center(
+                        child: Text(
+                      ">",
+                      style: TextStyle(color: Colors.white, fontSize: 22.0),
+                    ));
+                  },
+                  onWillAccept: (data) {
+                    print("will");
+                    setState(() {
+                      barColor2 = Colors.red;
+                    });
+                    return true;
+                  },
+                  onAccept: (data) {
+                    setState(() {
+                      barColor2 = Colors.red;
+                    });
+                  },
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+      // ),
     );
   }
 
-  Widget buildBody(ThemeProvider themeProvider) {
-    return Container(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Text(
-              'Alarm',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              title.split(':')[1],
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 100),
-          ],
-        ),
-      ),
-    );
+  buildFab() {
+    return Padding(
+        padding: EdgeInsets.only(bottom: 60),
+        child: ButtonTheme(
+            height: 100,
+            minWidth: 100,
+            child: RaisedButton(
+                child: Text(
+                  "Stop",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                color: Theme.of(context).accentColor,
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(70.0)))));
   }
 }
